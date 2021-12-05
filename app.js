14eb5118d422edbf1e9d1358ccc9c9f88cda89a5
Kiran Babu <kiranbabu7675@gmail.com>
2021-12-05 14:08:05 +0530

    Completed Project

---
+++ app.js
@@ -0,0 +1,30 @@
+const ipInput = document.querySelector("#ip-input");
+const cpInput = document.querySelector("#cp-input");
+const quantityInput = document.querySelector("#quantity");
+const CalculateBtn = document.querySelector("#calculate-btn");
+const output = document.querySelector("#output");
+
+function CalculateProfitOrLost() {
+  const initialPrice = Number(ipInput.value).toFixed(2);
+  const currentPrice = Number(cpInput.value).toFixed(2);
+  const quantity = Number(quantityInput.value);
+  //Profit Logic
+  if (initialPrice && quantity) {
+    if (currentPrice > initialPrice) {
+      const profit = currentPrice - initialPrice;
+      const profitPercentage = ((profit / initialPrice) * 100).toFixed(2);
+      const currentProfit = (profit * quantity).toFixed(2);
+      output.innerText = `Profit: ${currentProfit}\n\n Profit Percentage: ${profitPercentage} %`;
+      output.style.backgroundColor = "rgb(33, 248, 51)";
+    } else if (initialPrice > currentPrice) {
+      const loss = initialPrice - currentPrice;
+      const lossPercentage = ((loss / initialPrice) * 100).toFixed(2);
+      const currentLoss = (loss * quantity).toFixed(2);
+      output.innerText = `Loss: ${currentLoss}\n\n Loss Percentage: ${lossPercentage} %`;
+      output.style.backgroundColor = "rgb(253, 36, 36)";
+    }
+  } else {
+    output.innerText = "Initial Price and Quantity Should be more than 0";
+  }
+}
+CalculateBtn.addEventListener("click", CalculateProfitOrLost);
